d651c5e94b5a8df574c2e1299da34703dad5f943
Yous77130 <13012@holbertonstudents.com>
2026-02-24 13:09:17 +0000

    add file for task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée
+ *
+ * Return: Toujours 0 (Succès)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}

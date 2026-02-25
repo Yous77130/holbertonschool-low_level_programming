17f2e82292e68161fb6f6531e5303470b3864624
Yous77130 <13012holbertonstudents.com>
2026-02-25 12:06:09 +0000

    add file for task 7

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,28 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Fonctions de base */
 int _putchar(char c);
-
-/* Task 0: isupper */
-int _isupper(int c);
-
-/* Task 1: isdigit */
-int _isdigit(int c);
-
-/* Task 2: multiplication */
-int mul(int a, int b);
-
-/* Task 3: numbers 0-9 */
-void print_numbers(void);
-
-/* Task 4: numbers 0-9 except 2 and 4 */
-void print_most_numbers(void);
-
-/* Task 5: numbers 0-14, ten times */
-void more_numbers(void);
-
-/* Task 6: straight line */
+/* ... autres prototypes ... */
 void print_line(int n);
+void print_diagonal(int n); /* <--- VERIFIE CETTE LIGNE */
 
-#endif /* MAIN_H */
+#endif

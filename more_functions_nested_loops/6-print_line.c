fc3a6444221862f5bd71738aaff56ac954964293
Yous77130 <13012@holbertonstudents.com>
2026-02-25 11:33:58 +0000

    add file for task 6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -3,21 +3,19 @@
 /**
  * print_line - draws a straight line in the terminal
  * @n: number of times the character _ should be printed
+ *
+ * Return: void
  */
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int i;
+	int i;
 
+	if (n > 0)
+	{
 		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }

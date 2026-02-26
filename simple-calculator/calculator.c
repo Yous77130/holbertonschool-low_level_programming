acfd867be059aac9ebcb0bc31d9729a9cb9a4cda
Yous77130 <13012@holbertonstudents.com>
2026-02-26 11:20:37 +0000

    add file for task 2

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -7,40 +7,42 @@
 int main(void)
 {
 	int choice = -1;
+	int a, b, result;
 
 	printf("Simple Calculator\n");
 
 	while (choice != 0)
 	{
-		printf("\n1) Add\n");
-		printf("2) Subtract\n");
-		printf("3) Multiply\n");
-		printf("4) Divide\n");
-		printf("0) Quit\n");
+		printf("\n1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\n");
 		printf("Choice: ");
 
-		/* Lecture du choix */
 		if (scanf("%d", &choice) != 1)
-		{
-			/* Sortie de secours si l'utilisateur entre du texte */
 			break;
-		}
 
-		/* Validation du choix */
 		if (choice == 0)
 		{
 			printf("Bye!\n");
 		}
-		else if (choice >= 1 && choice <= 4)
+		else if (choice == 1)
 		{
-			/* Pour l'instant on ne fait rien, la Task 2 arrive */
+			/* Task 2: Addition */
+			printf("A: ");
+			scanf("%d", &a);
+			printf("B: ");
+			scanf("%d", &b);
+
+			result = a + b;
+			printf("Result: %d\n", result);
+		}
+		else if (choice >= 2 && choice <= 4)
+		{
+			/* À implémenter dans les prochaines tâches */
+			printf("Operation to be implemented\n");
 		}
 		else
 		{
-			/* Si le nombre n'est ni 0, ni entre 1 et 4 */
 			printf("Invalid choice\n");
 		}
 	}
-
 	return (0);
 }

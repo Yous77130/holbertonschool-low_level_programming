b5bdd715d155be171cfb1e4bbdd4042b5d416ff2
Yous77130 <13012@holbertonstudends.com>
2026-03-02 11:52:57 +0000

    add file for task 2

--- benchmarking/instrumentation_lab.c
+++ benchmarking/instrumentation_lab.c
@@ -16,9 +16,7 @@ static void build_dataset(void)
 {
     unsigned int state;
     int i;
-
     state = SEED_VALUE;
-
     for (i = 0; i < DATASET_SIZE; i++)
         dataset[i] = (int)(next_value(&state) % 100000);
 }
@@ -27,7 +25,6 @@ static void process_dataset(void)
 {
     int i;
     int v;
-
     for (i = 0; i < DATASET_SIZE; i++)
     {
         v = dataset[i];
@@ -42,11 +39,9 @@ static unsigned long reduce_checksum(void)
 {
     unsigned long sum;
     int i;
-
     sum = 0;
     for (i = 0; i < DATASET_SIZE; i++)
         sum = (sum * 131ul) + (unsigned long)dataset[i];
-
     return (sum);
 }
 
@@ -58,32 +53,25 @@ int main(void)
     clock_t start_process, end_process;
     clock_t start_reduce, end_reduce;
 
-    /* Mesure TOTAL début */
     start_total = clock();
 
-    /* Phase BUILD */
     start_build = clock();
     build_dataset();
     end_build = clock();
 
-    /* Phase PROCESS */
     start_process = clock();
     process_dataset();
     end_process = clock();
 
-    /* Phase REDUCE */
     start_reduce = clock();
     checksum = reduce_checksum();
     end_reduce = clock();
 
-    /* Mesure TOTAL fin */
     end_total = clock();
 
-    /* Utilisation de checksum pour éviter l'erreur 'unused variable' */
     if (checksum == 0ul)
         printf("impossible\n");
 
-    /* Affichage strict selon le contrat (%.6f) */
     printf("TOTAL seconds: %.6f\n", (double)(end_total - start_total) / CLOCKS_PER_SEC);
     printf("BUILD_DATA seconds: %.6f\n", (double)(end_build - start_build) / CLOCKS_PER_SEC);
     printf("PROCESS seconds: %.6f\n", (double)(end_process - start_process) / CLOCKS_PER_SEC);

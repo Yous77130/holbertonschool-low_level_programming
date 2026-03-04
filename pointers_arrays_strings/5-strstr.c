f14f11836bba556c77dfa9fc87f314a6a0e6d540
Yous77130 <13012@holbertonstudents.com>
2026-03-04 15:58:24 +0100

    add file for task 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+char *_strstr(char *haystack, char *needle)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while (haystack[i] != '\0')
+    {
+        j = 0;
+        while (needle[j] != '\0' && haystack[i + j] == needle[j])
+            j++;
+        if (needle[j] == '\0')
+            return (haystack + i);
+        i++;
+    }
+    return (NULL);
+}

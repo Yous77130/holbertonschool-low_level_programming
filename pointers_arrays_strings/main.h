0931cbb6966fe3a55fb397b2a300da19a1af3891
Yous77130 <13012@holbertonstudents.com>
2026-03-04 15:50:25 +0100

    add file for task 2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H
+#include <stddef.h>
 #define MAIN_H
 
 int _putchar(char c);
@@ -15,4 +16,5 @@ char *cap_string(char *s);
 char *leet(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif

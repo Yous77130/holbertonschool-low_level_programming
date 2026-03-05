c8dd94a4b2c8103ba8a7c59142d1651fb861ee96
Yous77130 <13012@holbertonstudents.com>
2026-03-05 12:08:55 +0100

    add file for task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,21 @@
 #include "main.h"
-
 /**
- * main - check the code
+ * puts2 - Prints other characters
+ * @str: Paramatre dentree
  *
- * Return: Always 0.
  */
-int main(void)
-{
-    char *str;
 
-    str = "0123456789";
-    puts2(str);
-    return (0);
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+	if (i % 2 == 0)
+	{
+	_putchar(str[i]);
+	}
+	i++;
+	}
+	_putchar('\n');
 }

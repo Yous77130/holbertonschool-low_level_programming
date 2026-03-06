58be3b124c0367a873c39cb0b5bbb8e5f27f2678
Yous77130 <13012@holbertonstudents.com>
2026-03-06 11:38:45 +0100

    add file for task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+
 /**
- * main - Prints the name of the program
- * @argc: Argument count
- * @argv: Argument vector (array of strings)
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
-	return (0);
+    (void)argc;  /* argc is unused */
+    printf("%s\n", argv[0]);
+    return 0;
 }
 

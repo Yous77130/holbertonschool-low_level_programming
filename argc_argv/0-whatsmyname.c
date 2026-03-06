eacc7cfeb73a98a52b6a4a48175fa30ef91bd01f
Yous77130 <13012@holbertonstudents.com>
2026-03-06 11:43:08 +0100

    add file for task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,11 +3,15 @@
 
 int main(int argc, char *argv[])
 {
+    (void)argc;  // évite le warning de variable non utilisée
+
+    // Obtenir seulement le nom du fichier, pas le chemin
     char *name = strrchr(argv[0], '/');
     if (name)
-        name++;  // sauter le '/'
+        name++;  // passer le '/'
     else
         name = argv[0];
+
     printf("%s\n", name);
     return 0;
 }

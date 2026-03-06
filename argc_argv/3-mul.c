8cce57707616b49068a33c58ef03e5ccaea94a37
Yous77130 <13012@holbertonstudents.com>
2026-03-06 13:14:04 +0100

    add file for task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - multplies two numbers
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-    return (0);
+	int a, b;
+
+	if (argc != 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
+	else
+	{
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	printf("%d\n", a * b);
+	return (0);
+	}
 }

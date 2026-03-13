652705afc3452c89834dc276f3dbe36c381bb744
Yous77130 <13012@holbertonstudents.com>
2026-03-13 11:23:29 +0100

    add file for task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,16 +1,14 @@
-cat > 0-whatsmyname.c << 'EOF'
 #include <stdio.h>
 
 /**
  * main - prints the name of the program
- * @argc: number of command line arguments
- * @argv: array of command line arguments
- * Return: Always 0 (Success)
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0
  */
 int main(int argc, char **argv)
 {
-	printf("%s\n", argv[argc - argc]);
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
-EOF
-

787785b3e21657c88df97ca5c0b1709e1855e930
Yous77130 <13012@holbertonstudents.com>
2026-03-17 10:07:09 +0100

    add file for trask 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -9,44 +9,41 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i;
-	int count;
 	char c;
 	int n;
 	float f;
 	char *s;
+	char *sep;
 
 	va_start(args, format);
 	i = 0;
-	count = 0;
+	sep = "";
 	while (format && format[i])
 	{
-		if (count > 0 && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
-			printf(", ");
-		if (format[i] == 'c')
+		switch (format[i])
 		{
+		case 'c':
 			c = va_arg(args, int);
-			printf("%c", c);
-			count++;
-		}
-		if (format[i] == 'i')
-		{
+			printf("%s%c", sep, c);
+			sep = ", ";
+			break;
+		case 'i':
 			n = va_arg(args, int);
-			printf("%d", n);
-			count++;
-		}
-		if (format[i] == 'f')
-		{
+			printf("%s%d", sep, n);
+			sep = ", ";
+			break;
+		case 'f':
 			f = va_arg(args, double);
-			printf("%f", f);
-			count++;
-		}
-		if (format[i] == 's')
-		{
+			printf("%s%f", sep, f);
+			sep = ", ";
+			break;
+		case 's':
 			s = va_arg(args, char *);
 			if (s == NULL)
 				s = "(nil)";
-			printf("%s", s);
-			count++;
+			printf("%s%s", sep, s);
+			sep = ", ";
+			break;
 		}
 		i++;
 	}

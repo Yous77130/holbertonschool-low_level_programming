d08ec1370b391daafaf56dea436f6317017dce1c
Yous77130 <13012@holbertonstudents.com>
2026-03-18 11:29:00 +0100

    add file for task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,19 +2,55 @@
 #include "variadic_functions.h"
 
 /**
+ * print_one - prints one argument based on type
+ * @type: the format character
+ * @args: the va_list pointer
+ * @sep: separator pointer
+ */
+static void print_one(char type, va_list *args, char **sep)
+{
+	char c;
+	int n;
+	float f;
+	char *s;
+
+	switch (type)
+	{
+	case 'c':
+		c = va_arg(*args, int);
+		printf("%s%c", *sep, c);
+		*sep = ", ";
+		break;
+	case 'i':
+		n = va_arg(*args, int);
+		printf("%s%d", *sep, n);
+		*sep = ", ";
+		break;
+	case 'f':
+		f = va_arg(*args, double);
+		printf("%s%f", *sep, f);
+		*sep = ", ";
+		break;
+	case 's':
+		s = va_arg(*args, char *);
+		if (s == NULL)
+			s = "(nil)";
+		printf("%s%s", *sep, s);
+		*sep = ", ";
+		break;
+	default:
+		break;
+	}
+}
+
+/**
  * print_all - prints anything based on format
  * @format: list of types (c=char, i=int, f=float, s=string)
- *
- * Return: void
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i;
-	char c;
-	int n;
-	float f;
-	char *s;
 	char *sep;
 
 	va_start(args, format);
@@ -22,33 +58,7 @@ void print_all(const char * const format, ...)
 	sep = "";
 	while (format && format[i])
 	{
-		switch (format[i])
-		{
-		case 'c':
-			c = va_arg(args, int);
-			printf("%s%c", sep, c);
-			sep = ", ";
-			break;
-		case 'i':
-			n = va_arg(args, int);
-			printf("%s%d", sep, n);
-			sep = ", ";
-			break;
-		case 'f':
-			f = va_arg(args, double);
-			printf("%s%f", sep, f);
-			sep = ", ";
-			break;
-		case 's':
-			s = va_arg(args, char *);
-			if (s == NULL)
-				s = "(nil)";
-			printf("%s%s", sep, s);
-			sep = ", ";
-			break;
-		default:
-			break;
-		}
+		print_one(format[i], &args, &sep);
 		i++;
 	}
 	va_end(args);

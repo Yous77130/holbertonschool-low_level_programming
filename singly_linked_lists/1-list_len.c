04ea10bc5d6578db1de392d901b40d9e00073c88
Yous77130 <13012@holbertonstudents.com>
2026-03-26 14:01:37 +0100

    add file for task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - returns number of elements in a list
+ * @h: pointer to the head of the list
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}

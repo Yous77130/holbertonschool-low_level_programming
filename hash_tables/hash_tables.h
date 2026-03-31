69b711e6cfa649865c452fc79c269596ab86fc41
Yous77130 <13012@holbertonstudents.com>
2026-03-31 11:22:46 +0200

    add file for task 1

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -3,12 +3,6 @@
 
 #include <stddef.h>
 
-/**
- * struct hash_node_s - Node of a hash table
- * @key: The key, string
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
- */
 typedef struct hash_node_s
 {
 	char *key;
@@ -16,11 +10,6 @@ typedef struct hash_node_s
 	struct hash_node_s *next;
 } hash_node_t;
 
-/**
- * struct hash_table_s - Hash table data structure
- * @size: The size of the array
- * @array: An array of size @size
- */
 typedef struct hash_table_s
 {
 	unsigned long int size;
@@ -28,6 +17,7 @@ typedef struct hash_table_s
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 int _putchar(char c);
 
 #endif /* HASH_TABLES_H */

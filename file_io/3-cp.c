219c27676e91a4a62b3a10e7fdb56e56ff56b563
Yous77130 <13012@holbertonstudents.com>
2026-04-03 15:52:12 +0200

    fix 3-cp betty style task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,35 +5,39 @@
 #include "main.h"
 
 /**
- * main - copies content of a file to another
- * @ac: argument count
+ * open_files - opens source and destination files
  * @av: argument vector
- * Return: 0 on success, exits with error code on failure
+ * @fd_from: pointer to source fd
+ * @fd_to: pointer to dest fd
  */
-int main(int ac, char **av)
+void open_files(char **av, int *fd_from, int *fd_to)
 {
-	int fd_from, fd_to;
-	char buf[1024];
-	ssize_t r, w;
-
-	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	fd_from = open(av[1], O_RDONLY);
-	if (fd_from == -1)
+	*fd_from = open(av[1], O_RDONLY);
+	if (*fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
+	*fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (*fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		close(fd_from);
+		close(*fd_from);
 		exit(99);
 	}
+}
+
+/**
+ * copy_content - copies content from fd_from to fd_to
+ * @av: argument vector
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ */
+void copy_content(char **av, int fd_from, int fd_to)
+{
+	char buf[1024];
+	ssize_t r, w;
+
 	while ((r = read(fd_from, buf, 1024)) > 0)
 	{
 		w = write(fd_to, buf, r);
@@ -52,6 +56,15 @@ int main(int ac, char **av)
 		close(fd_to);
 		exit(98);
 	}
+}
+
+/**
+ * close_files - closes file descriptors
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ */
+void close_files(int fd_from, int fd_to)
+{
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
@@ -62,5 +75,25 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
+}
+
+/**
+ * main - copies content of a file to another
+ * @ac: argument count
+ * @av: argument vector
+ * Return: 0 on success
+ */
+int main(int ac, char **av)
+{
+	int fd_from, fd_to;
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	open_files(av, &fd_from, &fd_to);
+	copy_content(av, fd_from, fd_to);
+	close_files(fd_from, fd_to);
 	return (0);
 }
